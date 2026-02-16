3a09bdb7911ebeab1b9d161eeac1a9af462c4180
Teek Chaudhary <teek.chaudhary@autodesk.com>
2026-02-16 11:43:40 -0500

    Initial commit - FastAPI backend

---
+++ main.py
@@ -0,0 +1,63 @@
+from functools import lru_cache
+from typing import Union
+import os
+
+from fastapi import FastAPI, Depends
+from fastapi.responses import PlainTextResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+# routers: comment out next line till create them
+from routers import todos
+
+import config
+
+app = FastAPI()
+
+# router: comment out next line till create it
+app.include_router(todos.router)
+
+
+#origins = [
+#    "http://localhost:3000",
+#    "https://todo-frontend-khaki.vercel.app/",
+#]
+
+# CORS configuration, needed for frontend development
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# global http exception handler, to handle errors
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    print(f"{repr(exc)}")
+    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
+
+# to use the settings
+@lru_cache()
+def get_settings():
+    return config.Settings()
+
+
+@app.get("/")
+def read_root(settings: config.Settings = Depends(get_settings)):
+    # print the app_name configuration
+    print(settings.app_name)
+    return "Hello World"
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
